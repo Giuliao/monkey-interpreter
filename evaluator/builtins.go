613e3b7f8082b079f6e76f447f434fd648e31986
Giuliao <wangguang16@outlook.com>
2024-09-25 16:47:38 +0800

    4.6 add puts function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "monkey-interpreter/object"
+import (
+	"fmt"
+	"monkey-interpreter/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -93,6 +96,15 @@ var builtins = map[string]*object.Builtin{
 			copy(newElements, arr.Elements)
 			newElements[length] = args[1]
 			return &object.Array{Elements: newElements}
+		},
+	},
+
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
